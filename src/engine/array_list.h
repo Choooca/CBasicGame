28a3642b71ef73a7646bd14d6e53d7f8d828923d
Paul <98259426+Choooca@users.noreply.github.com>
2024-05-30 13:28:54 +0200

    [ADD] Continuous collision detection

--- src/engine/array_list.h
+++ src/engine/array_list.h
@@ -13,3 +13,4 @@ typedef struct array_list
 Array_List *array_list_create(size_t item_size, size_t initial_capacity);
 size_t array_list_append(Array_List *list, void *item);
 void *array_list_get(Array_List *list, size_t index);
+u8 array_list_remove(Array_List *list, size_t index);
