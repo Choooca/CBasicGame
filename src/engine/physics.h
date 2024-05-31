6855a82e21bc12793e681566c864a86dc5800802
Paul <98259426+Choooca@users.noreply.github.com>
2024-05-31 21:23:56 +0200

    [ADD] Collision layer and mask

--- src/engine/physics.h
+++ src/engine/physics.h
@@ -5,38 +5,51 @@
 #include <linmath.h>
 #include "type.h"
 
+typedef struct hit Hit;
+typedef struct body Body;
+typedef struct static_body Static_Body;
+
+typedef void (*On_Hit)(Body *self, Body *other, Hit hit);
+typedef void (*On_Hit_Static)(Body *self, Static_Body *other, Hit hit);
+
 typedef struct aabb
 {
 	vec2 position;
 	vec2 half_size;
 } AABB;
 
-typedef struct body
+struct body
 {
 	AABB aabb;
 	vec2 velocity;
 	vec2 acceleration;
-} Body;
+	On_Hit on_hit;
+	On_Hit_Static on_hit_static;
+	u8 collision_layer;
+	u8 collision_mask;
+};
 
-typedef struct static_body
+struct static_body
 {
 	AABB aabb;
-} Static_Body;
+	u8 collision_layer;
+};
 
-typedef struct hit
+struct hit
 {
-	bool is_hit;
+	size_t other_id;
 	f32 time;
 	vec2 position;
 	vec2 normal;
-} Hit;
+	bool is_hit;
+};
 
 void physics_init(void);
 void physics_update(void);
-size_t physics_body_create(vec2 position, vec2 size);
+size_t physics_body_create(vec2 position, vec2 size, vec2 velocity, u8 collision_layer, u8 collision_mask, On_Hit on_hit, On_Hit_Static on_hit_static);
 Body *physics_body_get(size_t index);
 Static_Body *physics_static_body_get(size_t index);
-size_t physics_static_body_create(vec2 position, vec2 size);
+size_t physics_static_body_create(vec2 position, vec2 size, u8 collision_layer);
 bool physics_point_intersect_aabb(vec2 point, AABB aabb);
 bool physics_aabb_intersect_aabb(AABB a, AABB b);
 AABB aabb_minkowski_difference(AABB a, AABB b);
